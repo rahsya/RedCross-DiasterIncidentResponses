53801c33e52861a69753afd0d3d1e0f24624107e
Rahul <rsyamlal@gmail.com>
2013-06-23 11:37:20 -0500

    first commit of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedCrossIncidents::Application.config.secret_token = '1296f8b2f4202706fb054e07021b9cae3f70b6ec4a1149847a5c24ef696148b38a076e67a5148413a0392c544e100772441fdbfb69e46c1544cd022d0223c5c8'
